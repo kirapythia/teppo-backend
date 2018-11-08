ee9d16ea334bd18277679f53fa8a3f8015d22e4e
Liisa Lado-Villar <29202468+liisaespoo@users.noreply.github.com>
2018-11-08 15:59:43 +0200

    Add Dockerfile.dist, entrypoint-dist.sh and remove some obsolete or unneeded files

---
+++ entrypoint-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This file is used by Dockerfile
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+
+# if the first argument to `docker run` starts with `--`, the user is passing spring launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+    #Flyway commands here
+
+    # Run as exec so the application can receive any Unix signals sent to the container, e.g., Ctrl + C.
+    exec ./gradlew --no-daemon "$@"
+fi
+
+# As argument is not --env, assume user want to run his own process, for sample a `bash`
+# shell to explore this image
+exec "$@"
